e00d1ee1779bd4dbb6215259fa67ba049a1f4b8c
Gilles Di Guglielmo <gdigugli@gmail.com>
2012-06-04 15:19:37 +0200

    formating

--- ez18n-webapp/src/main/java/com/google/gwt/sample/stockwatcher/client/GreetingServiceAsync.java
+++ ez18n-webapp/src/main/java/com/google/gwt/sample/stockwatcher/client/GreetingServiceAsync.java
@@ -6,5 +6,5 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  * The async counterpart of <code>GreetingService</code>.
  */
 public interface GreetingServiceAsync {
-  void greetServer(String input, AsyncCallback<String> callback);
+    void greetServer(String input, AsyncCallback<String> callback);
 }
--- ez18n-webapp/src/main/java/com/google/gwt/sample/stockwatcher/client/StockPrice.java
+++ ez18n-webapp/src/main/java/com/google/gwt/sample/stockwatcher/client/StockPrice.java
@@ -2,44 +2,44 @@ package com.google.gwt.sample.stockwatcher.client;
 
 public class StockPrice {
 
-  private String symbol;
-  private double price;
-  private double change;
+    private String symbol;
+    private double price;
+    private double change;
 
-  public StockPrice() {
-  }
+    public StockPrice() {
+    }
 
-  public StockPrice(String symbol, double price, double change) {
-    this.symbol = symbol;
-    this.price = price;
-    this.change = change;
-  }
+    public StockPrice(String symbol, double price, double change) {
+        this.symbol = symbol;
+        this.price = price;
+        this.change = change;
+    }
 
-  public String getSymbol() {
-    return this.symbol;
-  }
+    public String getSymbol() {
+        return this.symbol;
+    }
 
-  public double getPrice() {
-    return this.price;
-  }
+    public double getPrice() {
+        return this.price;
+    }
 
-  public double getChange() {
-    return this.change;
-  }
+    public double getChange() {
+        return this.change;
+    }
 
-  public double getChangePercent() {
-    return 100.0 * this.change / this.price;
-  }
+    public double getChangePercent() {
+        return 100.0 * this.change / this.price;
+    }
 
-  public void setSymbol(String symbol) {
-    this.symbol = symbol;
-  }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
 
-  public void setPrice(double price) {
-    this.price = price;
-  }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-  public void setChange(double change) {
-    this.change = change;
-  }
+    public void setChange(double change) {
+        this.change = change;
+    }
 }
