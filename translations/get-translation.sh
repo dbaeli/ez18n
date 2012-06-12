f3a82c64e90555f7428ebe039eab75a35778eac4
Dimitri BAELI <dbaeli@gmail.com>
2012-06-13 00:37:00 +0200

    Tooling for crowdin.net

--- translations/get-translation.sh
+++ translations/get-translation.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-if [ -z "$1" -o -z "$2"]
+if [ -z "$2" ]
 then
    echo "Usage $0 <lang> <key>"
    exit 1
 fi
 
 LANG=$1
-KAY=$2
+KEY=$2
+
+#TODO Rebuild the package 
+#curl http://api.crowdin.net/api/project/ez18n-demo/export?key=$KEY
 
 echo Downloading LANG=$LANG
 wget http://api.crowdin.net/api/project/ez18n-demo/download/fr.zip?key=$KEY -O fr.zip
@@ -16,7 +19,7 @@ TDIR=${LANG}_`date +%s`
 mkdir $TDIR
 unzip $LANG.zip -d $TDIR
 
-echo Fix package
+echo Rename all .properties files to _$LANG.properties
 for FILE in `find $TDIR -name *.properties`
 do
  NEWNAME=`dirname $FILE`/`basename -s .properties $FILE`_$LANG.properties
@@ -27,4 +30,4 @@ cd $TDIR
 zip -r ../ez18n_$LANG.zip *
 
 echo prepared ez18n_$LANG.zip
-echo Remove $TDIR
+rm -rf $TDIR
