4f84b23fd73a490a5f3352664c044c104940f6d9
gdigugli <gdigugli@gmail.com>
2012-09-14 19:37:00 +0200

    updated tech slides for javaone

--- ez18n-base/src/main/java/org/ez18n/runtime/BundleFactory.java
+++ ez18n-base/src/main/java/org/ez18n/runtime/BundleFactory.java
@@ -3,8 +3,8 @@ package org.ez18n.runtime;
 import java.lang.annotation.Annotation;
 import java.util.ServiceLoader;
 
-public class MessageBundleFactory {
-	public static final <S> S getBundle(Class<S> service, Class<? extends Annotation> annotation) {
+public class BundleFactory {
+	public static final <S> S get(Class<S> service, Class<? extends Annotation> annotation) {
 		final ServiceLoader<S> loader = ServiceLoader.<S> load(service);
 		for (S bundle : loader) {
 			if (bundle.getClass().getAnnotation(annotation) != null)
--- ez18n-sample/src/main/java/org/ez18n/sample/PostCard.java
+++ ez18n-sample/src/main/java/org/ez18n/sample/PostCard.java
@@ -1,18 +1,26 @@
 package org.ez18n.sample;
 
+import org.ez18n.runtime.BundleFactory;
+import org.ez18n.runtime.Desktop;
+
 public class PostCard {
 
-  private final Messages messages;
+	private final Messages messages;
 
-  public PostCard(Messages resources) {
-    this.messages = resources;
-  }
+	public PostCard(Messages resources) {
+		this.messages = resources;
+	}
 
-  public String getText() {
-    return messages.loveMeTender();
-  }
+	public String getText() {
+		return messages.loveMeTender();
+	}
 
-  public String getLoveText(String name) {
-    return messages.doYouLove(name);
-  }
+	public String getLoveText(String name) {
+		return messages.doYouLove(name);
+	}
+
+	public static final void main(String... args) {
+		final Messages bundle = BundleFactory.get(Messages.class, Desktop.class);
+		System.out.println(bundle.doYouLove("Mum"));
+	}
 }
